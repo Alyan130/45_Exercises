6bb98de22398a4d3a3bdfe678ee8d7cb87f8bcab
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-03-31 12:50:28 -0700

    accessing names with message

---
+++ question_12/main.js
@@ -0,0 +1,4 @@
+var friends = ["Murtuza", "Daniyal", "Hassan"];
+console.log("Hello, how are you " + friends[0] + " ?");
+console.log("Hello, how are you " + friends[1] + " ?");
+console.log("Hello, how are you " + friends[2] + " ?");
---
+++ question_12/main.ts
@@ -0,0 +1,4 @@
+let friends:String[]=["Murtuza","Daniyal","Hassan"];
+console.log("Hello, how are you "+friends[0]+" ?");
+console.log("Hello, how are you "+friends[1]+" ?");
+console.log("Hello, how are you "+friends[2]+" ?");
