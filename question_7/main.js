9a5d4c1926f32d471cca6b7890de816daef93e4d
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-03-31 12:01:02 -0700

    operations

---
+++ question_7/main.js
@@ -0,0 +1,8 @@
+var addition = 4 + 4;
+var subtraction = 10 - 2;
+var multiplication = 2 * 4;
+var division = 16 / 2;
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division : " + division);
