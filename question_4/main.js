15f06c6163c859b134f9839a1d60c68aba64fab4
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-03-31 05:07:41 -0700

    quotes

---
+++ question_4/main.js
@@ -0,0 +1,3 @@
+var quote = "Do not pray an easy life, pray for the strength to endure a difficult one.";
+var author = "Bruce lee";
+console.log(author + " once said, " + '"' + quote + '"');
---
+++ question_4/main.ts
@@ -0,0 +1,3 @@
+let quote="Do not pray an easy life, pray for the strength to endure a difficult one.";
+let author="Bruce lee";
+console.log(author+" once said, "+'"'+quote+'"');
