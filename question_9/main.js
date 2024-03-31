fa0a56343adc2614a66c43f0304add9c8dbddc7e
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-03-31 12:17:02 -0700

    print favourite number

---
+++ question_9/main.js
@@ -0,0 +1,3 @@
+var favNum = 1;
+var message = "My favourite number is ".concat(favNum, ".");
+console.log(message);
---
+++ question_9/main.ts
@@ -0,0 +1,3 @@
+let favNum:number=1;
+let message:String=`My favourite number is ${favNum}.`;
+console.log(message);
