2a95b05c0bd5b00adb44a273d7414dd5e5ea6679
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-03-31 05:21:29 -0700

    famous person quote

---
+++ question_5/main.ts
@@ -0,0 +1,3 @@
+let famous_person:String="Bruce lee";
+let  messsage:String=`${famous_person} once said, "Do not pray an easy life, pray for the strength to endure a difficult one."`;
+console.log(messsage);
