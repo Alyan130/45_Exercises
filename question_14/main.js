985ddbd37bbb2abc375b8757e0b0e21ab1f6ca21
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-04-01 13:25:57 -0700

    inviting for dinner

---
+++ question_14/main.js
@@ -0,0 +1,4 @@
+var peoples = ["Mawia", "Areeb", "Hamza"];
+console.log("Hello ".concat(peoples[0], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[1], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[2], ", join us for a delicious dinner and delightful conversation tonight"));
---
+++ question_14/main.ts
@@ -0,0 +1,4 @@
+let peoples:String[]=["Mawia","Areeb","Hamza"];
+console.log(`Hello ${peoples[0]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[1]}, join us for a delicious dinner and delightful conversation tonight`)
+console.log(`Hello ${peoples[2]}, join us for a delicious dinner and delightful conversation tonight`)
