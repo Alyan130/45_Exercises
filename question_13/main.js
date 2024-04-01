75645b7dcd3e9e891ac1936eb7a9ca71b663e164
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-04-01 13:18:01 -0700

    transportation array

---
+++ question_13/main.js
@@ -0,0 +1,5 @@
+var car = ["Audi A3", "Honda Civic", "BMW M3", "Rolls Royce"];
+console.log("\"I would like to own a ".concat(car[0], ".\""));
+console.log("\"I would like to own a ".concat(car[1], ".\""));
+console.log("\"I would like to own a ".concat(car[2], ".\""));
+console.log("\"I would like to own a ".concat(car[3], ".\""));
---
+++ question_13/main.ts
@@ -0,0 +1,5 @@
+let car:String[]=["Audi A3","Honda Civic","BMW M3","Rolls Royce"];
+console.log(`"I would like to own a ${car[0]}."`);
+console.log(`"I would like to own a ${car[1]}."`);
+console.log(`"I would like to own a ${car[2]}."`);
+console.log(`"I would like to own a ${car[3]}."`);
