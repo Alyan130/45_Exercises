728c8312efe3acfc04a5c9ff84465babb56ceca6
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-04-08 11:22:05 -0700

    new invitation

---
+++ question_16/main.js
@@ -0,0 +1,9 @@
+var peoples = ["Daniyal", "Mawia", "Zamad", "Areeb", "Murtuza"];
+console.log("Now bigger table is found!");
+peoples.push("Salman");
+console.log("Hello ".concat(peoples[0], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[1], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[2], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[3], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[4], ", join us for a delicious dinner and delightful conversation tonight"));
+console.log("Hello ".concat(peoples[5], ", join us for a delicious dinner and delightful conversation tonight"));
---
+++ question_16/main.ts
@@ -0,0 +1,9 @@
+let peoples:String[]=["Daniyal","Mawia","Zamad","Areeb","Murtuza"];
+console.log("Now bigger table is found!")
+peoples.push("Salman");
+console.log(`Hello ${peoples[0]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[1]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[2]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[3]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[4]}, join us for a delicious dinner and delightful conversation tonight`);
+console.log(`Hello ${peoples[5]}, join us for a delicious dinner and delightful conversation tonight`);
