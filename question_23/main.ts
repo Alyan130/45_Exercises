435ca5a6c0790a24f25d12f6e24502a3a9ec9d0a
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-04-16 04:17:08 -0700

    predictions

---
+++ question_23/main.ts
@@ -0,0 +1,41 @@
+let number=10;
+
+//test1
+console.log("Is number == 10? I predict True.");
+console.log(number==10);
+
+//test2
+console.log("Is number == 40? I predict True.");
+console.log(number==40);
+
+//test3
+console.log("Is number-6=1? I predict False.");
+console.log(number-6==1);
+
+//test4
+console.log("Is number < 3? I predict True.");
+console.log(number<3);
+
+//test5
+console.log("Is number+1 == 11? I predict False.");
+console.log(number+1 == 11);
+
+//test6
+console.log("Is number - 2==8? I predict False.");
+console.log(number-2==8);
+
+//test7
+console.log("Is number-- == 15? I predict False.");
+console.log(number-- == 15);
+
+//test8
+console.log("Is number+20 == 30? I predict True.");
+console.log(number+20 == 30);
+
+//test9
+console.log("Is number>6? I predict False.");
+console.log(number>6);
+
+//test10
+console.log("Is number <= 20? I predict True.");
+console.log(number <= 20);
---
+++ question_24/main.ts
@@ -0,0 +1,28 @@
+let colour="Green";
+console.log("Is colour == Green? I predict True.");
+console.log(colour=="Green");
+
+console.log("Is colour != Green? I predict True.");
+console.log(colour!="Green");
+
+let planet="JUPITER";
+console.log("Is planet.toLowercase() == JUPITER? I predict False.");
+console.log(planet.toLowerCase() == "JUPITER");
+
+console.log("Is planet.toLowercase == jupiter? I predict True.");
+console.log(planet.toLowerCase() =="jupiter");
+
+let number=15;
+console.log("Is number != 15? I predict True.");
+console.log(number!=15);
+
+console.log("Is number == 15? I predict False.");
+console.log(number==15);
+
+console.log("Is number < 3? I predict True.");
+console.log(number<3);
+
+
+
+
+
