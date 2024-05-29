0e753c9ae2a5242e7549274f41a38bcc1582fbc8
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-29 21:19:46 +0500

    if_else chain

---
+++ question_27/main.js
@@ -0,0 +1,30 @@
+var alien_colour1 = "Green";
+if (alien_colour1 == "Green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_colour1 == "Yellow") {
+    console.log("Player just earned 10 points.");
+}
+else {
+    console.log("Player just earned 15 points.");
+}
+alien_colour1 = "Yellow";
+if (alien_colour1 == "Green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_colour1 == "Yellow") {
+    console.log("Player just earned 10 points.");
+}
+else {
+    console.log("Player just earned 15 points.");
+}
+alien_colour1 = "Red";
+if (alien_colour1 == "Green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_colour1 == "Yellow") {
+    console.log("Player just earned 10 points.");
+}
+else {
+    console.log("Player just earned 15 points.");
+}
---
+++ question_27/main.ts
@@ -0,0 +1,33 @@
+let alien_colour="Green";
+
+if(alien_colour=="Green"){
+    console.log("Player just earned 5 points.")
+}
+else if(alien_colour=="Yellow"){
+    console.log("Player just earned 10 points.")
+}else{
+    console.log("Player just earned 15 points.")
+}
+
+
+alien_colour="Yellow";
+
+if(alien_colour=="Green"){
+    console.log("Player just earned 5 points.")
+}
+else if(alien_colour=="Yellow"){
+    console.log("Player just earned 10 points.")
+}else{
+    console.log("Player just earned 15 points.")
+}
+
+alien_colour="Red";
+
+if(alien_colour=="Green"){
+    console.log("Player just earned 5 points.")
+}
+else if(alien_colour=="Yellow"){
+    console.log("Player just earned 10 points.")
+}else{
+    console.log("Player just earned 15 points.")
+}
