a43a54f3117cc721ae3df5eafd6343db33583172
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-29 22:20:26 +0500

    fruits array

---
+++ question_29/main.js
@@ -0,0 +1,18 @@
+var favourite_fruits = ["Mango", "Banana", "Apricot"];
+for (var i = 0; i < favourite_fruits.length; i++) {
+    if (favourite_fruits[i] == "Strawberry") {
+        console.log("You really like Strawberry!");
+    }
+    if (favourite_fruits[i] == "Mango") {
+        console.log("You really like Mango!");
+    }
+    if (favourite_fruits[i] == "Peach") {
+        console.log("You really like Peach!");
+    }
+    if (favourite_fruits[i] == "Banana") {
+        console.log("You really like Banana!");
+    }
+    if (favourite_fruits[i] == "Apricot") {
+        console.log("You really like Apricot!");
+    }
+}
---
+++ question_29/main.ts
@@ -0,0 +1,20 @@
+let favourite_fruits=["Mango","Banana","Apricot"];
+
+for(let i=0;i<favourite_fruits.length;i++){
+    
+if(favourite_fruits[i]=="Strawberry"){
+    console.log("You really like Strawberry!")
+}
+if(favourite_fruits[i]=="Mango"){
+    console.log("You really like Mango!")
+}
+if(favourite_fruits[i]=="Peach"){ 
+    console.log("You really like Peach!")
+}
+if(favourite_fruits[i]=="Banana"){
+    console.log("You really like Banana!");
+}
+if(favourite_fruits[i]=="Apricot"){
+    console.log("You really like Apricot!")
+}
+}
