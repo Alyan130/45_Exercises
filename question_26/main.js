97496cdd21cbec366b48297d3560c309238fd6ff
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-29 21:09:28 +0500

    if_else

---
+++ question_26/main.js
@@ -0,0 +1,14 @@
+var alienColour = "Green";
+if (alienColour == "Green") {
+    console.log("Player just earned 5 points.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
+var alien_colour = "Yellow";
+if (alien_colour == "Green") {
+    console.log("Player just earned 5 points.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
---
+++ question_26/main.ts
@@ -0,0 +1,14 @@
+let alienColour="Green";
+
+if(alienColour=="Green"){
+    console.log("Player just earned 5 points.");
+}else{
+    console.log("Player just earned 10 points.")
+}
+
+let alien_colour="Yellow";
+if(alien_colour=="Green"){
+    console.log("Player just earned 5 points.");
+}else{
+    console.log("Player just earned 10 points.")
+}
