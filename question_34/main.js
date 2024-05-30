47cb6c9262c3418229dd1ec130b38e4b8c811656
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-30 18:45:59 +0500

    pizza array

---
+++ question_34/main.js
@@ -0,0 +1,5 @@
+var pizzas = ["fajita pizza", "bbq pizza", "cheese pizza"];
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like ".concat(pizzas[i], "."));
+}
+console.log("Pizza is my favourite meal.");
---
+++ question_34/main.ts
@@ -0,0 +1,6 @@
+let pizzas=["fajita pizza","bbq pizza","cheese pizza"];
+
+for(let i=0;i<pizzas.length;i++){
+    console.log(`I like ${pizzas[i]}.`);
+}
+console.log("Pizza is my favourite meal.");
