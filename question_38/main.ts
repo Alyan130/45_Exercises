05020c07f643768e507dae84392937a96eee1c6e
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-30 19:27:55 +0500

    functions

---
+++ question_38/main.ts
@@ -0,0 +1,6 @@
+function describe_city(city:String,country="Pakistan"){
+console.log(`${city} is in ${country}.`)
+}
+describe_city("Hyderabad");
+describe_city("Karachi");
+describe_city("London","England")
