868b45276029a52ed7ef757dc46ecb9f5670e045
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-30 19:08:08 +0500

    function call

---
+++ question_36/main.js
@@ -0,0 +1,4 @@
+function make_shirt(size, text) {
+    console.log("The size of the shirt is ".concat(size, " and message printed on it is \"").concat(text, "\"."));
+}
+make_shirt("meduim", "hardwork is key");
---
+++ question_36/main.ts
@@ -0,0 +1,4 @@
+function make_shirt(size,text){
+console.log(`The size of the shirt is ${size} and message printed on it is "${text}".`);
+}
+make_shirt("meduim","hardwork is key");
