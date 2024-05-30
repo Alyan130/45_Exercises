7fd250df37795e7b7049405488ef5ac244207cfe
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-05-30 18:54:13 +0500

    animals array

---
+++ question_35/main.js
@@ -0,0 +1,6 @@
+var animals = ["lion", "tiger", "leopard"];
+for (var i = 0; i < animals.length; i++) {
+    console.log("".concat(animals[i], " is a wild animal."));
+}
+console.log("All these animals have sharp claws and tail.");
+console.log("These aniamls are carnivores.");
---
+++ question_35/main.ts
@@ -0,0 +1,7 @@
+let animals=["lion","tiger","leopard"];
+
+for(let i=0;i<animals.length;i++){
+    console.log(`${animals[i]} is a wild animal.`);
+}
+console.log("All these animals have sharp claws and tail.");
+console.log("These animals are carnivores.")
