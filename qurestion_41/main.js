f7da66b7e6d994771ee46281648f6a39fc625dcf
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-06-01 00:24:31 +0500

    array function

---
+++ qurestion_41/main.js
@@ -0,0 +1,7 @@
+var magician_names = ["David Copperfield", "Harry Houdini", "Penn Jillette", "David Blaine"];
+function show_magician(magicianNames) {
+    magicianNames.forEach(function (e) {
+        console.log(e);
+    });
+}
+show_magician(magician_names);
---
+++ qurestion_41/main.ts
@@ -0,0 +1,9 @@
+let magician_names=["David Copperfield","Harry Houdini","Penn Jillette","David Blaine"];
+
+function show_magician(magicianNames:String[]){
+magicianNames.forEach(e=>{
+    console.log(e);
+});
+}
+
+show_magician(magician_names);
